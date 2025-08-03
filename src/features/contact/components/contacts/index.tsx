930cad5fa9fe8d7dc5004cb2cf84b5cc1f081a65
temirhanb <temirhan023@gmail.com>
2025-08-03 13:21:26 +0500

    - complete base api

--- src/features/contact/components/contacts/index.tsx
+++ src/features/contact/components/contacts/index.tsx
@@ -1,9 +1,13 @@
-export const Contacts = ()=>{
-  return(
+import {useProfileStore} from "@/state/providers/provider-profile";
+
+export const Contacts = () => {
+  const {data: {telegram}} = useProfileStore(state => state);
+
+  return (
     <div
       className={"mx-[16px] text-[17px] border-t border-[#00000033] h-[60px] flex flex-row items-center justify-between"}>
       <span>Мой телеграм</span>
-      <span className={"font-semibold"}>@ribakit3</span>
+      <span className={"font-semibold"}>{telegram === undefined ? "@ribakit3" : telegram}</span>
     </div>
-  )
-}
+  );
+};
--- src/features/contact/components/description/index.tsx
+++ src/features/contact/components/description/index.tsx
@@ -1,15 +1,20 @@
-export const Description = ()=>{
-  return(
+import {useProfileStore} from "@/state/providers/provider-profile";
+
+export const Description = () => {
+  const {data: {about}} = useProfileStore(state => state);
+
+  return (
     <div
       className={"p-[16px]"}
     >
       <div
         className={"text-[17px] relative font-normal leading-[22px]"}
-      >Я профессиональный скуф, обажаю сидеть дома и часто играю в Мафию с друзьями по вечера в Сицилии и зовем всех
-        желающ и разные другие дела...
+      >{about === undefined ?
+        "Я профессиональный скуф, обажаю сидеть дома и часто играю в Мафию с друзьями по вечера в Сицилии и зовем всехжелающ и разные другие дела..."
+        : about}
         <img className={"absolute right-0 pointer bottom-0"} src="/next-text.svg" alt="full text"/>
       </div>
     </div>
 
-  )
-}
+  );
+};
